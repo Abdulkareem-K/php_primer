3dbcce5e5c6088810c4e5dee4f5f18eb29b3f402
Abdulkareem-K <72037843+Abdulkareem-K@users.noreply.github.com>
2022-11-29 11:21:27 +0300

    Added some button
    Index page has been updated with two new buttons

--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 $title = "Index"; 
 include 'includes\header.php' ?>
 
-<button class="btn btn-primary" type="submit">Button</button>    
+<button class="btn btn-primary" type="submit">Button</button>  
+
+<button class="btn btn-primary" type="Second">Button</button> 
+<button class="btn btn-primary" type="Third">Button</button> 
 <?php require 'includes/footer.php' ?> 
